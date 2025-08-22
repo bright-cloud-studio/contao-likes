a48456d9b4822d81548e089ab9af23f2e0f9e4ea
Bright Cloud Studio <mark@brightcloudstudio.com>
2025-08-22 14:39:00 -0400

    Create BcsLikesBundle.php

---
+++ src/BcsLikesBundle.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @copyright  Bright Cloud Studio
+ * @author     Bright Cloud Studio
+ * @package    Contao Likes
+ * @license    LGPL-3.0+
+ * @see	       https://github.com/bright-cloud-studio/contao-likes
+ */
+
+namespace Bcs\LikesBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class BcsLikesBundle extends Bundle
+{
+}
