13f0411099501751c1fa63faed737bf18da93a10
Bright Cloud Studio <mark@brightcloudstudio.com>
2025-08-22 14:56:18 -0400

    Create config.php

---
+++ src/Resources/contao/config/config.php
@@ -0,0 +1,16 @@
+<?php
+
+//use Bcs\OSIBundle\FormMultipleChoiceQuestion;
+//use Bcs\MultiChoiceWizard;
+
+// Module to display a Test's results
+//$GLOBALS['FE_MOD']['osi']['mod_test_results'] = 'Bcs\Module\ModTestResults';
+
+// Customized version of Contao's Form Checkbox field
+//$GLOBALS['TL_FFL']['multiple_choice_question'] = 'Bcs\OSIBundle\FormMultipleChoiceQuestion';
+
+// Customized version of the "OptionWizard" Contao Backend Form Widget
+//$GLOBALS['BE_FFL']['multi_choice_wizard'] = 'Bcs\MultiChoiceWizard';
+
+// Hook when form is submitted
+//$GLOBALS['TL_HOOKS']['processFormData'][]      = array('Bcs\Hooks\FormHooks', 'onSubmitTest');
