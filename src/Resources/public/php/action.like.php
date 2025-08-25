a3909f75c356714da226967809bfda93568e9261
Bright Cloud Studio <mark@brightcloudstudio.com>
2025-08-25 14:09:47 -0400

    Create action.like.php

---
+++ src/Resources/public/php/action.like.php
@@ -0,0 +1,24 @@
+<?php
+    
+    // Start PHP session and include Composer, which also brings in our Google Sheets PHP stuffs
+    session_start();
+    require_once $_SERVER['DOCUMENT_ROOT'] . '/../vendor/autoload.php';
+    
+    // Load our database connection information from a txt file on the root of the server, for safety
+    $serializedData = file_get_contents($_SERVER['DOCUMENT_ROOT'] . '/../db.txt');
+    $db_info = unserialize($serializedData);
+    
+    // Connect to database using our privately hidden information
+    $dbh = new mysqli("localhost", $db_info[0], $db_info[1], $db_info[2]);
+    if ($dbh->connect_error) {
+        die("Connection failed: " . $dbh->connect_error);
+    }
+    
+    $myfile = fopen("logs/psy_work_form_save_meeting_required_log_".date('m-d-Y_hia').".txt", "w") or die("Unable to open file!");
+    
+    // Get data from ajax
+    $unique_id = $_POST['assignment_id'];
+    
+    fwrite($myfile, "SAVING: Query Results: " . $result_update . "\r\n");
+    fclose($myfile);
+    echo "success";
