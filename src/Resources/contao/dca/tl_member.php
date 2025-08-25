8aad9d24f2fbfc9a62a14f46a04c36bf2120f114
Bright Cloud Studio <mark@brightcloudstudio.com>
2025-08-25 15:45:23 -0400

    Update tl_member.php

--- src/Resources/contao/dca/tl_member.php
+++ src/Resources/contao/dca/tl_member.php
@@ -2,17 +2,20 @@
 
    /* Extend the tl_user palettes */
   foreach ($GLOBALS['TL_DCA']['tl_member']['palettes'] as $k => $v) {
-      $GLOBALS['TL_DCA']['tl_member']['palettes'][$k] = str_replace('groups;', 'groups;{price_tier_legend}, price_tier, price_tier_display;{admin_review_legend}, last_reviewed; {last_review_and_submit_legend}, last_review_and_submit;', $v);
+      $GLOBALS['TL_DCA']['tl_member']['palettes'][$k] = str_replace('groups;', 'groups;{contao_likes_legend}, likes;', $v);
   }
 
   /* Add fields to tl_user */
-  $GLOBALS['TL_DCA']['tl_member']['fields']['price_tier'] = array
+  $GLOBALS['TL_DCA']['tl_member']['fields']['likes'] = array
   (
-    'label'                   => &$GLOBALS['TL_LANG']['tl_member']['price_tier'],
-    'inputType'               => 'select',
-    'flag'                    => DataContainer::SORT_ASC,
-    'eval'                    => array('mandatory'=>true, 'multiple'=>false, 'tl_class'=>'', 'submitOnChange'=>false),
-    'options_callback'	      => array('Bcs\Backend\ServiceBackend', 'getPriceTiers'),
-    'sql'                     => "varchar(50) NOT NULL default ''",
-    'default'                 => "tier_1_price"
+        'label'            => &$GLOBALS['TL_LANG']['tl_member_group']['likes'],
+        'inputType'        => 'checkboxWizard',
+        'eval'             => array('multiple'=> true, 'mandatory'=>false, 'tl_class'=>'long'),
+        'flag'             => DataContainer::SORT_ASC,
+        'options_callback' => array('Bcs\Backend\LikesBackend', 'getLikes'),
+        'sql'              => "blob NULL",
+        'save_callback' => array
+        (
+            array('Bcs\Backend\LikesBackend', 'saveCallback')
+        ),
   );
