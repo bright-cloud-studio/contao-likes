5e191b84d3f671e364ae8ccf7a497adef2e601b5
Bright Cloud Studio <mark@brightcloudstudio.com>
2025-08-27 09:47:58 -0400

    Update tl_member.php

--- src/Resources/contao/dca/tl_member.php
+++ src/Resources/contao/dca/tl_member.php
@@ -3,22 +3,22 @@
 use Contao\DataContainer;
 use Contao\DC_Table;
 
-   /* Extend the tl_user palettes */
-  foreach ($GLOBALS['TL_DCA']['tl_member']['palettes'] as $k => $v) {
-      $GLOBALS['TL_DCA']['tl_member']['palettes'][$k] = str_replace('groups;', 'groups;{contao_likes_legend}, likes;', $v);
-  }
+/* Extend the tl_user palettes */
+foreach ($GLOBALS['TL_DCA']['tl_member']['palettes'] as $k => $v) {
+  $GLOBALS['TL_DCA']['tl_member']['palettes'][$k] = str_replace('groups;', 'groups;{contao_likes_legend}, likes;', $v);
+}
 
-  /* Add fields to tl_user */
-  $GLOBALS['TL_DCA']['tl_member']['fields']['likes'] = array
-  (
-        'label'            => &$GLOBALS['TL_LANG']['tl_member_group']['likes'],
-        'inputType'        => 'checkboxWizard',
-        'eval'             => array('multiple'=> true, 'mandatory'=>false, 'tl_class'=>'long'),
-        'flag'             => DataContainer::SORT_ASC,
-        'options_callback' => array('Bcs\Backend\LikesBackend', 'getLikes'),
-        'sql'              => "blob NULL",
-        'save_callback' => array
-        (
-            array('Bcs\Backend\LikesBackend', 'saveCallback')
-        ),
-  );
+/* Add fields to tl_user */
+$GLOBALS['TL_DCA']['tl_member']['fields']['likes'] = array
+(
+    'label'            => &$GLOBALS['TL_LANG']['tl_member_group']['likes'],
+    'inputType'        => 'checkboxWizard',
+    'eval'             => array('multiple'=> true, 'mandatory'=>false, 'tl_class'=>'long'),
+    'flag'             => DataContainer::SORT_ASC,
+    'options_callback' => array('Bcs\Backend\LikesBackend', 'getLikes'),
+    'sql'              => "blob NULL",
+    'save_callback' => array
+    (
+        array('Bcs\Backend\LikesBackend', 'saveCallback')
+    ),
+);
