41bc89d4a29823ecda12834c3701777d1979f882
Bright Cloud Studio <mark@brightcloudstudio.com>
2025-08-27 13:29:38 -0400

    Update tl_settings.php

--- src/Resources/contao/dca/tl_settings.php
+++ src/Resources/contao/dca/tl_settings.php
@@ -2,11 +2,11 @@
 
 use Contao\Config;
 
-$GLOBALS['TL_DCA']['tl_settings']['palettes']['default'] = str_replace('{files_legend', '{contao_likes_legend}, icon;{files_legend', $GLOBALS['TL_DCA']['tl_settings']['palettes']['default']);
+$GLOBALS['TL_DCA']['tl_settings']['palettes']['default'] = str_replace('{files_legend', '{contao_likes_legend}, contao_likes_icon;{files_legend', $GLOBALS['TL_DCA']['tl_settings']['palettes']['default']);
 
 $GLOBALS['TL_DCA']['tl_settings']['fields'] += [
-    'icon' => [
-        'label'             => &$GLOBALS['TL_LANG']['tl_settings']['icon'],
+    'contao_likes_icon' => [
+        'label'             => &$GLOBALS['TL_LANG']['tl_settings']['contao_likes_icon'],
         'inputType'         => 'text',
         'default'           => '<i class="fas fa-heart"></i>',
         'eval'              => ['mandatory' => 'true', 'tl_class' => 'w50'],
