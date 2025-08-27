7dbc0e5c816f46c3cfa2bd83a7c1d91cc649de8b
Bright Cloud Studio <mark@brightcloudstudio.com>
2025-08-27 13:34:17 -0400

    Update LikesBackend.php

--- src/Resources/contao/library/Bcs/Backend/LikesBackend.php
+++ src/Resources/contao/library/Bcs/Backend/LikesBackend.php
@@ -41,30 +41,6 @@ class LikesBackend extends Backend
 
 
     public function saveCallback($varValue, DataContainer $dc) {
-        
-        // Get the Test IDs
-        $group_ids = unserialize($varValue);
-
-        // Foreach Test
-        foreach($group_ids as $id) {
-            
-            // Get any Test Results that are assigned to this Test
-            $test_results = TestResult::findBy(['test = ?'], [$id]);
-            if($test_results) {
-                foreach($test_results as $test_result) {
-                    
-                    // Get any current Member Group values as an array
-                    $mem_groups = unserialize($test_result->member_groups);
-                    // Add our current Member Group to the list of selected Member Groups
-                    $mem_groups[] = $dc->activeRecord->id;
-                    // Save our changes
-                    $test_result->member_groups = serialize($mem_groups);
-                    $test_result->save();
-                }
-            }
-            
-        }
-
         return $varValue;
     }
 
@@ -72,14 +48,6 @@ class LikesBackend extends Backend
     
     // Get Members as options for a Select DCA field
     public function getLikes(DataContainer $dc) {
-        $tests = array();
-        $this->import('Database');
-        $result = $this->Database->prepare("SELECT * FROM tl_form WHERE formType='test' ORDER BY title ASC")->execute();
-        while($result->next())
-        {
-            $tests = $tests + array($result->id => $result->title . '[' . $result->id . ']');   
-        }
-        return $tests;
     }
     
     
